1ef5894b4035f2b314023c87f37e88d10447f780
Eizo491 <202380450@psu.palawan.edu.ph>
2026-04-08 08:42:05 +0800

    update

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,20 +1,18 @@
-var staticCacheName = "hangarin-v6"; // Bumped to v6 to include offline handler scripts
+var staticCacheName = "hangarin-v7"; 
 var filesToCache = [
     '/', 
     '/static/css/style.css',
     '/static/js/main.js',
     '/static/js/offline_handler.js',
-    '/task/create/', // Ensure your "Add Task" URL is cached so the form opens offline
+    '/task/create/', 
     '/static/img/icon-192.png', 
     '/static/img/icon-512.png',
 ];
 
-// Cache assets on install
 self.addEventListener("install", function (e) {
     self.skipWaiting(); 
     e.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
-            // We use a map to try caching files individually so one failure doesn't kill the worker
             return Promise.all(
                 filesToCache.map(function(url) {
                     return cache.add(url).catch(err => console.warn("Could not cache:", url));
@@ -24,7 +22,6 @@ self.addEventListener("install", function (e) {
     );
 });
 
-// Remove old caches
 self.addEventListener("activate", function (e) {
     e.waitUntil(
         caches.keys().then(function (cacheNames) {
@@ -43,17 +40,18 @@ self.addEventListener("fetch", function (event) {
     if (!event.request.url.startsWith(self.location.origin)) return;
     if (event.request.method !== 'GET') return;
 
-    if (event.request.url.pathname === '/' || event.request.url.includes('/task/')) {
+    const url = new URL(event.request.url);
+
+    if (url.pathname === '/' || url.pathname.includes('/task/')) {
         event.respondWith(
             fetch(event.request)
                 .then(function (response) { 
-                        return caches.open(staticCacheName).then(function (cache) {
+                    return caches.open(staticCacheName).then(function (cache) {
                         cache.put(event.request, response.clone());
                         return response;
                     });
                 })
                 .catch(function () {
-              
                     return caches.match(event.request);
                 })
         );
